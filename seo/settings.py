632ec5c7382fd603ef9804f8da560d35a68a3b07
community-vercel <community-vercel>
2025-02-20 12:41:20 -0800

    aaa

--- seo/settings.py
+++ seo/settings.py
@@ -186,8 +186,8 @@ USE_TZ = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8081',
 ]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8081']  # Adjust for your frontend domain
-CORS_ALLOWED_ORIGINS = ["http://localhost:8081", "http://127.0.0.1:8081"]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8081',"https://portal.sharplogicians.com"]  # Adjust for your frontend domain
+CORS_ALLOWED_ORIGINS = ["http://localhost:8081", "http://127.0.0.1:8081","https://portal.sharplogicians.com"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
